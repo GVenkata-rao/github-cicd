1f9567fa54c4e6a397a338684df1cfa0fa692e2e
GVenkata-rao <venkatg526@gmail.com>
2025-07-21 16:44:21 +0530

    testing

--- src/main/java/com/cicd/CICDController.java
+++ src/main/java/com/cicd/CICDController.java
@@ -10,14 +10,4 @@ public class CICDController {
 	public String getText() {
 		return "Welcome to CICDController.";
 	}
-	
-	/**
-	 * echo "# github-cicd" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/GVenkata-rao/github-cicd.git
-git push -u origin main
-	 */
 }
