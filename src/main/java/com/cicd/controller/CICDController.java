ab292d4079763b15edb77325e54ef592fb2a4e4e
GVenkata-rao <venkatg526@gmail.com>
2025-07-22 16:17:03 +0530

    Changed package structure and project Name

--- src/main/java/com/cicd/controller/CICDController.java
+++ src/main/java/com/cicd/controller/CICDController.java
@@ -1,4 +1,4 @@
-package com.cicd;
+package com.cicd.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CICDController {
 
-	@GetMapping
+	@GetMapping("/cicd")
 	public String getText() {
 		return "Welcome to CICDController";
 	}
